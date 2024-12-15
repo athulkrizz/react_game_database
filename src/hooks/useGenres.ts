f5966f4ada46de89f6a110a8fd0c8a7829812356
Athul <athulkrishnan.1108@gmail.com>
2024-12-15 15:06:59 +0530

    Refactor code - moved entity interfaces

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -2,14 +2,9 @@ import { useQuery } from "@tanstack/react-query";
 import ms from "ms";
 import genres from "../data/genres";
 import APIClient from "../services/api-client";
+import { Genres } from "../entities/Genres";
 
 const apiClient = new APIClient<Genres>("/genres");
-
-export interface Genres {
-    id: number;
-    name: string;
-    image_background: string;
-}
 
 const useGenres = () =>
     useQuery({
